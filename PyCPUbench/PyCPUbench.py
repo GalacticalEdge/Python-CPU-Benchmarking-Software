3417acb9a3bc2854b8de71bb438b176e8bfd6cb3
Galactical Edge <31458662+GalacticalEdge@users.noreply.github.com>
2022-12-10 10:36:23 -0800

    0.0.2
    This update comes with the following new things:
    
    More options for the number of calculations - Users can now select from three options—"Short" which will make the program calculate up to 1,000,000,000. "Medium" which will make the program calculate up to 5,000,000,000. Lastly, "Long" will make the program calculate 10,000,000,000. I may consider adding more user customizability to the number of calculations in the future.
    
    Code optimizations and changes - I have made various updates and optimizations to the program's code, which help increase its speed and readability.

--- PyCPUbench/PyCPUbench.py
+++ PyCPUbench/PyCPUbench.py
@@ -19,8 +19,22 @@ elif device_OS == "":
     device_OS = "Unknown operating system"
 
 print("Welcome to the Python CPU Benchmark Program! In this program, your computer's single-threaded performance will be tested by doing various math calculations.")
-
 time.sleep(5)
+while True:
+    lengthinput = input("How long do you want the test to be?\nType \"Short\" for calculations up to 1,000,000,000.\nType \"Medium\" for calculations up to 5,000,000,000.\nType \"Long\" for calculations up to 10,000,000,000.\nPlease input your answer: ")
+    if lengthinput == "Short":
+        testlength = 1000000000
+        break
+    elif lengthinput == "Medium":
+        testlength = 5000000000
+        break
+    elif lengthinput == "Long":
+        testlength = 10000000000
+        break
+    else:
+        print("Please input a valid answer.")
+        continue
+
 count = 11
 for i in range(10):
     count -= 1
@@ -28,7 +42,8 @@ for i in range(10):
     time.sleep(1)
 
 start = datetime.datetime.now()
-for a in range(5000000000):
+
+for a in range(testlength):
     if a%10 == 0:
         if a%8 == 0:
             if a%6 == 0:
@@ -38,13 +53,13 @@ for a in range(5000000000):
 
 end = datetime.datetime.now()
 results = end - start
-print("Benchmark started at: ", end="")
-print(start.strftime("%X"))
-print("Benchmark ended at: ", end="")
-print(end.strftime("%X"))
-print("Benchmark took: ", end="")
-print(results)
-print("System CPU: ", end="")
-print(device_CPU)
-print("Operating System: ", end="")
-print(device_OS)
+start = str(start)
+end = str(end)
+results = str(results)
+
+print(f"Benchmark length: {lengthinput}")
+print(f"Benchmark started at: {start}")
+print(f"Benchmark ended at: {end}")
+print(f"Benchmark took: {results}")
+print(f"System CPU: {device_CPU}")
+print(f"Operating System: {device_OS}")
