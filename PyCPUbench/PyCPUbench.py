da84d9c358d9468a9b5d06c54f391df3d81abbf1
Galactical Edge <31458662+GalacticalEdge@users.noreply.github.com>
2023-01-11 17:53:27 -0800

    Update PyCPUbench.py

--- PyCPUbench/PyCPUbench.py
+++ PyCPUbench/PyCPUbench.py
@@ -1,11 +1,6 @@
 import datetime
 import time
 import platform as deviceinfo
-import threading
-import _thread
-from tkinter import *
-
-# Note: The threading and _thread modules are stubs. These will be used to add multi-threaded benchmarks in the future
 
 device_CPU = deviceinfo.processor()
 if device_CPU == "":
@@ -29,49 +24,42 @@ while True:
     lengthinput = input("How long do you want the test to be?\nType \"Short\" for calculations up to 1,000,000,000.\nType \"Medium\" for calculations up to 5,000,000,000.\nType \"Long\" for calculations up to 10,000,000,000.\nPlease input your answer: ")
     if lengthinput == "Short":
         testlength = 1000000000
+        break
     elif lengthinput == "Medium":
         testlength = 5000000000
+        break
     elif lengthinput == "Long":
         testlength = 10000000000
+        break
     else:
         print("Please input a valid answer.")
         continue
 
-    count = 11
-    for i in range(10):
-        count -= 1
-        print(f"The benchmark will begin in {count} seconds")
-        time.sleep(1)
+count = 11
+for i in range(10):
+    count -= 1
+    print(f"The benchmark will begin in {count} seconds")
+    time.sleep(1)
 
-    start = datetime.datetime.now()
+start = datetime.datetime.now()
 
-    for a in range(testlength):
-        if a%10 == 0:
-            if a%8 == 0:
-                if a%6 == 0:
-                    if a %4 == 0:
-                        if a%2 == 0:
-                            print(a)
+for a in range(testlength):
+    if a%10 == 0:
+        if a%8 == 0:
+            if a%6 == 0:
+                if a %4 == 0:
+                    if a%2 == 0:
+                        print(a)
 
-    end = datetime.datetime.now()
-    results = end - start
-    start = str(start)
-    end = str(end)
-    results = str(results)
+end = datetime.datetime.now()
+results = end - start
+start = str(start)
+end = str(end)
+results = str(results)
 
-    print(f"Benchmark length: {lengthinput}")
-    print(f"Benchmark started at: {start}")
-    print(f"Benchmark ended at: {end}")
-    print(f"Benchmark took: {results}")
-    print(f"System CPU: {device_CPU}")
-    print(f"Operating System: {device_OS}")
-    
-    while True:
-        yesorno = input("Would you like to continue? Y/n?: ")
-        if yesorno == "Y" or yesorno == "y":
-            break
-        elif yesorno == "N" or yesorno == "n":
-            exit()
-        else:
-            print("Please enter a valid response")
-            continue
+print(f"Benchmark length: {lengthinput}")
+print(f"Benchmark started at: {start}")
+print(f"Benchmark ended at: {end}")
+print(f"Benchmark took: {results}")
+print(f"System CPU: {device_CPU}")
+print(f"Operating System: {device_OS}")
